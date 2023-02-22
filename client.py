f3e15d6ec864d98b26fc025ec04711869ffd8904
j00lie <joonas.liedes@student.lut.fi>
2023-02-22 12:17:26 +0000

    wikipedia api implementation

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import xmlrpc.client
 from datetime import datetime
 
-proxy = xmlrpc.client.ServerProxy("http://localhost:8000")
+proxy = xmlrpc.client.ServerProxy("http://localhost:8000", allow_none=True)
 
 user_input = ""
 while user_input != "quit":
@@ -28,16 +28,23 @@ while user_input != "quit":
             else:
                 for note in notes:
                     print(note)
-
+        elif user_input == "3":
+            topic = input("Enter the name of the topic to append to: ")
+            wikipedia_info = proxy.get_wikipedia_info(topic)
+            if wikipedia_info:
+                proxy.append_to_topic(topic, wikipedia_info)
+                print(f"Successfully appended Wikipedia info to topic {topic}")
+            else:
+                print("No Wikipedia info found for the given topic")
         elif user_input == "quit":
             pass
         else:
             print("Invalid input. Please enter a valid option.")
     except xmlrpc.client.ProtocolError as err:
         print(f"Protocol error occurred: {err}")
-    except xmlrpc.client.Fault as err:
-        print(f"Server error occurred: {err.faultString}")
+    # except xmlrpc.client.Fault as err:
+    #     print(f"Server error occurred: {err.faultString}")
     except ConnectionRefusedError as err:
         print(f"Connection error occurred: {err}")
-    except Exception as err:
-        print(f"An error occurred: {err}")
+    # except Exception as err:
+    #     print(f"An error occurred: {err}")
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from xmlrpc.server import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
-import threading
+import requests
 from socketserver import ThreadingMixIn
 import xml.etree.ElementTree as ET
+import json
 
 
 class NoteServer:
@@ -50,6 +51,7 @@ class NoteServer:
             new_timestamp = ET.Element("timestamp")
             new_timestamp.text = timestamp
             new_note.append(new_timestamp)
+
             new_topic.append(new_note)
 
             # Write the updated XML to the file
@@ -60,9 +62,59 @@ class NoteServer:
         except Exception as e:
             print(f"Error in save_notes: {e}")
 
+    def append_to_topic(self, topic_name, link):
+        tree = ET.parse(self.xml_file)
+        root = tree.getroot()
+        # Check if the topic already exists
+        existing_topic = root.find(f"./topic[@name='{topic_name}']")
+        if existing_topic is None:
+            return None
+        else:
+            # Add Wikipedia information to the topic
+            if link:
+                wikipedia = ET.Element("wikipedia")
+                # wikipedia_summary = ET.Element("summary")
+                # wikipedia_summary.text = summary
+                wikipedia_link = ET.Element("link")
+                wikipedia_link.text = link
+                # wikipedia.append(wikipedia_summary)
+                wikipedia.append(wikipedia_link)
+                existing_topic.append(wikipedia)
+                tree.write(self.xml_file, xml_declaration=True)
+
+    def get_wikipedia_info(self, topic_name):
+        # Send a GET request to the Wikipedia API
+
+        URL = "https://en.wikipedia.org/w/api.php"
+
+        PARAMS = {
+            "action": "opensearch",
+            "search": topic_name,
+            "limit": "1",
+            "format": "json",
+        }
+
+        response = requests.get(url=URL, params=PARAMS)
+
+        # Check if the response was successful
+        if response.status_code == 200:
+            data = response.json()
+            # print(data)
+            # summary = data["extract"]
+            full_link = data[-1][0]
+            return full_link
+        else:
+            return None
+
+
+class MyRequestHandler(SimpleXMLRPCRequestHandler):
+    def __init__(self, request, client_address, server):
+        self.allow_none = True
+        super().__init__(request, client_address, server)
+
 
 class ThreadedXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
-    def __init__(self, addr, requestHandler=SimpleXMLRPCRequestHandler):
+    def __init__(self, addr, requestHandler=MyRequestHandler):
         SimpleXMLRPCServer.__init__(self, addr, requestHandler=requestHandler)
         self.allow_reuse_address = True
 
